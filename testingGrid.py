a095c04dfd8deaeec3741d0033bfe5e426ffc826
Beto-23 <mansilla_alberto@outlook.com>
2020-09-27 01:30:45 -0300

    Plotting Points

--- testingGrid.py
+++ testingGrid.py
@@ -13,14 +13,24 @@ xx = np.arange(start = 0, stop = 3, step = 1) #[0,1,2]
 yy = np.arange(start = 0, stop = 3, step = 1) #[0,1,2]
 X, Y = np.meshgrid(xx,yy)
 
+y = [0.56422, 1.77284, 1.52623]
+z = [0.15, 0.3, 0.45]
+n = [0, 0, 1, 1, 0]
+
 b = []
 Z = [[0, 1, 1], [1, 1, 1], [0, 0, 1]] # predicción
 # correlación con la grilla 00 01 02 10 11 12 20 21 22
-print(Z)
-print(xx)
-print(yy)
+# print(Z)
+# print(xx)
+# print(yy)
 
 plt.contourf(X, Y, Z, cmap = ListedColormap(('red', 'green'))) 
+
+# Aplicando un arreglo de colores con itertools (para probar, descomentar la importación)
+# colors = itertools.cycle(["r", "b", "g"])
+# plt.subplot().scatter(z, y, color=next(colors))
+plt.subplot().scatter(z, y, color='red')
+
 plt.show()
 
 """
