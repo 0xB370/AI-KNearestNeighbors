1142f2a9f79cfd7f6f2b0cf3c0beed563d555ba5
Beto-23 <mansilla_alberto@outlook.com>
2020-10-12 16:21:01 -0300

    Basic Tie Treatment

--- plotter.py
+++ plotter.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from knnPure import KnnClasifier
 import matplotlib.patches as mpatches
+import collections as col
 
 # Paleta de colores para la gráfica
 paleta_colores = ('r', 'b', 'g', 'c', 'm', 'y', 'k', 'w')
@@ -19,16 +19,16 @@ def init_plot(x_range=None, y_range=None, x_label="$x_1$", y_label="$x_2$"):
   x_label -- string
   y_label -- string
   """
-  _, ax = plt.subplots(dpi=70)
+  _, ejes = plt.subplots(dpi=70)
   # Definición del estilo y color del grid
-  ax.grid(c='0.70', linestyle=':')
+  ejes.grid(c='0.70', linestyle=':')
   # Definición del rango de los ejes
-  ax.set_xlim(x_range) 
-  ax.set_ylim(y_range)
+  ejes.set_xlim(x_range) 
+  ejes.set_ylim(y_range)
   # Definición de labels
-  ax.set_xlabel(x_label)
-  ax.set_ylabel(y_label)
-  return ax
+  ejes.set_xlabel(x_label)
+  ejes.set_ylabel(y_label)
+  return ejes
 
 
 
@@ -43,7 +43,6 @@ class kNearestNeighbors():
     """Predicción de clase para cada elemento de x
     x -- (N x D)
     """
-    predictions = []
     if isinstance(self.y_train[0], np.ndarray):
       flat_list = []
       for sublist in self.y_train:
@@ -51,6 +50,7 @@ class kNearestNeighbors():
               flat_list.append(item)
       self.y_train = flat_list
     nof_classes = np.amax(self.y_train) + 1
+    predictions = []
     for x_test in x:
       # Array de distancias entre el punto de prueba actual (x_test) y todos los puntos de "entrenamiento"
       distances = np.sum(np.abs(self.x_train - x_test), axis=1)
@@ -66,8 +66,14 @@ class kNearestNeighbors():
         neighbor_label = self.y_train[neighbor_id]
         # Actualización del arreglo de votos
         votes[neighbor_label] += 1
-      # El label predecido es el que tiene la mayor cantidad de votos
-      predictions.append(np.argmax(votes))
+      # El label predicho es el que tiene la mayor cantidad de votos
+      # En caso de haber empate, se tomará la clase del punto de menor distancia
+      conteo = col.Counter(votes)
+      if (conteo[np.max(votes)] > 1):
+        predictions.append(self.y_train[np.argmin(distances)])
+      else:
+        predictions.append(np.argmax(votes))
+      # predictions.append(np.argmax(votes))
     return predictions
 
 
@@ -92,22 +98,21 @@ class kAnalysis():
     # Inicialización de clasificador
     self.nn = kNearestNeighbors(x, y, k)
    
-  def prepare_test_samples(self, low=0, high=2, step=0.01):
+  def prepare_test_samples(self, min=0, max=2, step=0.01):
     """Generación del grid con los puntos de prueba"""
     # Definición del rango
-    self.range = [low, high]
-    # start with grid of points from [low, high] x [low, high]
-    # Definición del grid de [low, high] x [low, high] puntos
+    self.range = [min, max]
+    # Definición del grid de [min, max] x [min, max] puntos
     # Ejemplos de np.mgrid:
     # np.mgrid[0:7:5] => array([0, 5])
     # np.mgrid[0:15:5] => array([ 0,  5, 10])
     # np.mgrid[0:15:2] => array([ 0,  2,  4,  6,  8, 10, 12, 14])
-    grid = np.mgrid[low:high+step:step, low:high+step:step]
+    grilla = np.mgrid[min:max+step:step, min:max+step:step]
     # Conversión a array de puntos bidimensionales
     # np.vstack apila arrays verticalmente
     # np.vstack(([1,2,3],[2,3,4])) = array([[1, 2, 3],
     #                                       [2, 3, 4]])
-    self.x_test = np.vstack([grid[0].ravel(), grid[1].ravel()]).T
+    self.x_test = np.vstack([grilla[0].ravel(), grilla[1].ravel()]).T
 
   def analyse(self):
     """Ejecución del clasificador sobre los puntos de prueba y separación de los mismos de acuerdo a las respectivas etiquetas"""
@@ -115,12 +120,12 @@ class kAnalysis():
     self.y_test = self.nn.predict(self.x_test)
     self.classified = []
     # Iteración sobre los labels disponibles
-    for label in range(self.nof_classes):
+    for tag in range(self.nof_classes):
       # Si el i-ésimo label == label actual -> add test[i]
-      class_i = np.array([self.x_test[i] \
-                          for i, l in enumerate(self.y_test) \
-                          if l == label])
-      self.classified.append(class_i)    
+      clasificacion_i = np.array([self.x_test[i] \
+                          for i, t in enumerate(self.y_test) \
+                          if t == tag])
+      self.classified.append(clasificacion_i)
 
   def plot(self, t='', K=5, etiquetas=[], x_label="X", y_label="Y"):
     """Visualización de los resultados de la clasificación"""
@@ -136,8 +141,6 @@ class kAnalysis():
     legends.append(kValue)
     print(legends)
     plt.legend(handles=[*legends], loc='upper right')
-    """ for i, x in enumerate(self.classified_prueba):
-      plot.plot(*x.T, paleta_colores[i] + '^') """
     # Pintando la grilla
     for i, x in enumerate(self.classified):
       plot.plot(*x.T, paleta_colores[i] + ',')
@@ -148,33 +151,6 @@ class kAnalysis():
 class Plotter: 
     def plotKnnGraphic(self, *tupleToPrint, K, minValue, maxValue, step, etiquetas, x_label, y_label):
       knn = kAnalysis(*tupleToPrint, k=K)
-      knn.prepare_test_samples(low=minValue, high=maxValue, step=step)
+      knn.prepare_test_samples(min=minValue, max=maxValue, step=step)
       knn.analyse()
       knn.plot(t='KNN Classifier', K=K, etiquetas=etiquetas, x_label=x_label, y_label=y_label)
-
-
-
-
-      
-
-
-
-
-"""
-l1 = Analysis(X1, X2, X3, distance=0)
-# l1.prepare_test_samples(low=(-1), high=3, step=0.01)
-l1.prepare_test_samples(low=-1, high=2, step=0.01)
-l1.analyse()
-l1.plot()
-
-plt.show()
-"""
-
-""" # apply kNN with k=1 on the same set of training samples
-# Con k=39 ya se comienza a romper y con k=40 ya se va de tema
-knn = kAnalysis(X1, X2, X3, k=5, distance=0)
-knn.prepare_test_samples(low=-1, high=3, step=0.02)
-knn.analyse()
-# knn.analyse_prueba([[0.5, 0.5], [0.5, 1], [0.5, 1.5], [1, 0.5], [1, 1], [1, 1.5], [1.5, 0.5], [1.5, 1], [1.5, 1.5]])
-knn.plot()
-plt.show() """
